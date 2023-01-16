8b2b557ad5784ff5fa0d935e68af8abba18e6694
a.hodaee <a.hodaee@ebcom.ir>
2023-01-16 12:58:08 +0330

    ✨ added theme

---
+++ src/configs/theme.js
@@ -0,0 +1,89 @@
+import { createTheme } from "@mui/material/styles";
+
+const themeConfig = createTheme({
+  breakpoints: {
+    values: {
+      xxs: 0,
+      xs: 400,
+      sm: 576,
+      md: 768,
+      lg: 992,
+      xl: 1280,
+    },
+  },
+  palette: {
+    primary: {
+      main: "#84A98C",
+      light: "#CAD2C5",
+    },
+
+    secondary: {
+      main: "#354F52",
+    },
+
+    background: {
+      default: "#fff",
+    },
+  },
+
+  components: {
+    MuiSkeleton: {
+      defaultProps: {
+        animation: "wave",
+      },
+    },
+    MuiTextField: {
+      defaultProps: {
+        inputProps: {
+          autoComplete: "off",
+        },
+      },
+    },
+
+    MuiDrawer: {
+      defaultProps: {
+        keepMounted: false,
+      },
+    },
+
+    // custum button styles
+    MuiButton: {
+      styleOverrides: {
+        contained: {
+          borderRadius: "1rem",
+          color: "white",
+          boxShadow: "none !important",
+          fontSize: "0.75rem  ",
+          fontWeight: "600",
+          height: "2.5rem",
+        },
+        secondary: {
+          background: "#fff",
+          borderRadius: "1rem",
+          boxShadow: "none !important",
+          color: "#f80",
+          height: "2.5rem",
+        },
+        outlined: {
+          color: "#354F52",
+          border: `1px solid #354F52`,
+          borderRadius: "1rem",
+          fontSize: "0.75rem  ",
+          fontWeight: "600",
+          height: "2.5rem",
+        },
+
+        text: {
+          backgroundColor: "transparent !important",
+          boxShadow: "none",
+          color: "#354F52",
+          fontSize: "0.75rem  ",
+          fontWeight: "600",
+          height: "2.5rem",
+        },
+      },
+    },
+  },
+});
+
+export default themeConfig;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import routes from "./configs/routes";
+import { ThemeProvider } from "@mui/material/styles";
+
 import reportWebVitals from "./reportWebVitals";
+
+import themeConfig from "./configs/theme";
 import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const router = createBrowserRouter(routes);
-
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router}>
+  <ThemeProvider theme={themeConfig}>
+    <Router>
       <App />
-    </RouterProvider>
-  </React.StrictMode>
+    </Router>
+  </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
