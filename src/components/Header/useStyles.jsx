b3ac95b2251f98604db9ed4a96e33d331e9f6d50
a.hodaee <a.hodaee@ebcom.ir>
2023-01-16 17:28:37 +0330

    💄 header redesign

--- src/components/Header/useStyles.jsx
+++ src/components/Header/useStyles.jsx
@@ -1,19 +1,35 @@
 import { makeStyles } from "@mui/styles";
 
+import background from "../../assets/images/background.png";
+
 const useStyles = makeStyles((theme) => ({
   header: {
-    position: "fixed",
-    top: 0,
-    width: "100vw",
-    height: "6rem",
+    backgroundColor: theme.palette.primary.light,
+    backgroundImage: `url(${background})`,
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "100% 100%",
+    backgroundSize: "50%",
+    height: "50vh",
+    padding: "3rem 0.5rem",
+    minHeight: "30rem",
+
+    [theme.breakpoints.down("sm")]: {
+      backgroundSize: "contain",
+      height: "100vh",
+    },
   },
 
   social: {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "100%",
     display: "flex",
     justifyContent: "space-between",
     alignItems: "center",
     backgroundColor: theme.palette.secondary.main,
     padding: "0.25rem",
+    zIndex: "100",
 
     "& > a": {
       display: "flex",
@@ -31,13 +47,18 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     justifyContent: "space-between",
     alignItems: "center",
-    backgroundColor: theme.palette.primary.main,
+    backgroundColor: theme.palette.primary.light,
+    boxShadow: "0 0 8px 0px #cad2c5",
     borderRadius: "1rem",
-    margin: "1rem auto",
+    margin: "0.5rem auto",
     padding: "0.5rem",
-    maxWidth: "36rem",
+    maxWidth: "768px",
 
-    "& a": {
+    "& > a": {
+      color: theme.palette.secondary.main,
+    },
+
+    "& .MuiButton-root a": {
       color: "#fff",
     },
   },
