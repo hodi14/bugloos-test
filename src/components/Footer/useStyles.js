ddc02433f06be9be874ccbdc34927924ed69806d
a.hodaee <a.hodaee@ebcom.ir>
2023-01-17 16:32:14 +0330

    💄 better layout when content is empty

--- src/components/Footer/useStyles.js
+++ src/components/Footer/useStyles.js
@@ -3,6 +3,7 @@ import { makeStyles } from "@mui/styles";
 const useStyles = makeStyles((theme) => ({
   footer: {
     backgroundColor: theme.palette.secondary.main,
+    margin: "auto 0 0",
     padding: "0.5rem",
     textAlign: "center",
 
